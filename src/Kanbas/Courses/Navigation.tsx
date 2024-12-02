54314af962b92c4b083b4a1282bf11c688562da2
manhasv <51957730+manhasv@users.noreply.github.com>
2024-12-02 13:43:59 -0500

    fix module bug

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,15 +1,14 @@
 import { Link, useLocation, useParams } from "react-router-dom";
-import { courses } from "../Database";
+
 export default function CoursesNavigation() {
   const { pathname } = useLocation();
   const { cid } = useParams();
-  const course = courses.find((course) => course._id === cid);
-  console.log(course?.name);
+  
   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
-        <Link key={link} to={`/Kanbas/Courses/${course && course._id}/${link}`} className={`list-group-item  border border-0
+        <Link key={link} to={`/Kanbas/Courses/${cid}/${link}`} className={`list-group-item  border border-0
           ${pathname.includes(link) ? "active" : "text-danger"}`}>
           {link}
         </Link>
